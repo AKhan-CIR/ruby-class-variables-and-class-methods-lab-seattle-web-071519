408e7796386dbe347264221e7c0919a6e44798c2
Asif Khan <akhan@codeinreach.com>
2019-07-21 20:22:31 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+@@count = 0
+@@artist = []
+@@genre = []
+
+    def initialize(song_name, artist, genre)
+        @@count +=1
+        @song_name = song_name
+        @artist = artist
+        @genre = genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artist
+        @@
+
+
+end
